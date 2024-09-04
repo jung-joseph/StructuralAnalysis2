f93fed8cbfbc7c6cd6623a3367e6a655c9ef24a3
Joseph Jung <joseph.jung@icloud.com>
2024-09-04 12:03:01 -0600

    Added Verification Problems

---
+++ StructuralAnalysis2/HelperFiles/json.swift
@@ -0,0 +1,8 @@
+//
+//  json.swift
+//  StructuralAnalysis2
+//
+//  Created by Joseph Jung on 9/2/24.
+//
+
+import Foundation
---
+++ StructuralAnalysis2/VerificationProblems/VerificationProblemsView.swift
@@ -0,0 +1,18 @@
+//
+//  VerificationProblemsView.swift
+//  StructuralAnalysis2
+//
+//  Created by Joseph Jung on 9/2/24.
+//
+
+import SwiftUI
+
+struct VerificationProblemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VerificationProblemsView()
+}
